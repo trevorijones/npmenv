a0920e69c74bf36945786564f8e5fa7ef5c40717
CBO <c.borges@castsoftware.com>
2017-10-20 13:27:02 +0000

    Use nvm to install node in this directory
    Either choose node version to install from the .node-version or
    export a NODE_VERSION variable
    
    Nvm is installed to .nvm under this directory
    Node is installed to .nvm/versions/node/<version>
    
    Use bin/init.sh to set up node with nvm
    
    You can source bin/_env under your terminal shell to have the nvm
    command after installation
    
    Many versions of node can be installed, for that change version in
    .node_version or export NODE_VERSION and run bin/init.sh again

--- bin/init.sh
+++ bin/init.sh
@@ -3,13 +3,15 @@ __DIR__=$(dirname $(readlink -f $0))
 . $__DIR__/_env
 
 # Node Version Manager & Node
-if [ ! -f "${NVM_DIR}/nvm.sh" ]
+if [ ! -s "$NVM_DIR/nvm.sh" ]
 then
     curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh |  bash
-    # source nvm 
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-    nvm install "$NODE_VERSION"
-
 fi
+
+if [ -n "$NODE_VERSION" ]
+then
+    nvm install "$NODE_VERSION"
+fi
+
 
